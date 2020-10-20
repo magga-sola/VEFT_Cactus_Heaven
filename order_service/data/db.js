27bd9c699a119592ce111227d2524aa3f1de72eb
m4gga <margriette123@gmail.com>
2020-10-20 17:46:39 +0000

    bjo til mongodb dotid og connectadi

--- order_service/data/db.js
+++ order_service/data/db.js
@@ -2,7 +2,7 @@ const orderSchema = require('./schema/order');
 const orderItemSchema = require('./schema/orderItem');
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('<add-your-connection-string-here>', {
+const connection = mongoose.createConnection('mongodb+srv://eva:eva@cactusheavencluster.6lte1.mongodb.net/CactusHeavenDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
