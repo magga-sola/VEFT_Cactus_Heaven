caf20b9d7d43e4502af3bac29ddb12e29dd5347f
m4gga <margriette123@gmail.com>
2020-10-21 00:00:01 +0000

    NET core dót

---
+++ logging_service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace logging_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
