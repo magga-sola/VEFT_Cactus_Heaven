6eb9ec559ff74d4e74896508150b2824850feb24
m4gga <margriette123@gmail.com>
2020-10-23 22:26:03 +0000

    lagadi stoopid villu

--- order_service/index.js
+++ order_service/index.js
@@ -55,8 +55,7 @@ const createOrder = async (order_object) => {
             totalPrice: finalPrice,
             orderDate: today
         })
-        
-        createOrderItems(order_object, newOrder);
+        return newOrder;
 
     } catch(err) {
         throw (err);
@@ -64,14 +63,13 @@ const createOrder = async (order_object) => {
 }
 
 const createOrderItems = async (order_object, new_order) => {
-    console.log("the Json object: ", order_object);
-    console.log("and the new order: ", new_order);
+    
     try {
+        let orderItemList = [];
         let items = order_object.items;
 
-        for ( let i = 0; i < items; i++) {
-            const rowPrice = await (items[i].quantity * items[i].unitPrice);
-
+        for ( let i = 0; i < items.length; i++) {
+            const rowPrice = (items[i].quantity * items[i].unitPrice);
             const newOrderItem = await dbOrderItem.create({
                 description: items[i].description,
                 quantity: items[i].quantity,
@@ -79,8 +77,9 @@ const createOrderItems = async (order_object, new_order) => {
                 rowPrice: rowPrice,
                 orderId: new_order._id
             })
+            orderItemList.push(newOrderItem);
         }
-        return; 
+        return orderItemList; 
     } catch (err) {
         throw(err);
     }
@@ -94,136 +93,12 @@ const createOrderItems = async (order_object, new_order) => {
     const { orderQueue } = messageBrokerInfo.queues;
     const { output } = messageBrokerInfo.routingKeys;
 
-    channel.consume(orderQueue, data => {
+    channel.consume(orderQueue, async data => {
         const dataJson = JSON.parse(data.content.toString());
-        const newOrder = createOrder(dataJson);
-        createOrderItems(dataJson, newOrder);
-
+        const newOrder = await createOrder(dataJson);
+        const orderList = await createOrderItems(dataJson, newOrder);
         channel.publish(order, output, new Buffer(JSON.stringify(newOrder)));
 
         //console.log(`[x] Sent: ${JSON.stringify(dataJson)}`);
     }, { noAck: true });
 })().catch(e => console.error(e));
-
-// const amqp = require('amqplib/callback_api');
-// const dbOrder = require('./data/db').Order;
-// const dbOrderItem = require('./data/db').OrderItem;
-
-
-// const messageBrokerInfo = {
-//     exchanges: {
-//         order: 'order_exchange'
-//     },
-//     queues: {
-//         orderQueue: 'order_queue'
-//     },
-//     routingKeys: {
-//         input: 'create_order',
-//         output: 'order_created'
-//     }
-// }
-
-// const createMessageBrokerConnection = () => new Promise((resolve, reject) => {
-//     amqp.connect('amqp://localhost', (err, conn) => {
-//         if (err) { reject(err); }
-//         resolve(conn);
-//     });
-// });
-
-// const createChannel = connection => new Promise((resolve, reject) => {
-//     connection.createChannel((err, channel) => {
-//         if (err) { reject(err); }
-//         resolve(channel);
-//     });
-// });
-
-// const configureMessageBroker = channel => {
-//     const { orderQueue } = messageBrokerInfo.queues;
-//     const { createOrder } = messageBrokerInfo.routingKeys;
-
-
-//     Object.values(messageBrokerInfo.exchanges).forEach(val => {
-//         channel.assertExchange(val, 'direct', { durable: true });
-//         channel.assertQueue(orderQueue, { durable: true});
-//         channel.bindQueue(orderQueue, val, createOrder);
-//     });
-
-// };
-
-
-
-// const createOrder = async (order_object) => {
-
-//     let finalPrice = 0;
-
-//     order_object.items.forEach( item => {
-//         finalPrice += (item.quantity * item.unitPrice);
-//     });
-
-//     const today = new Date();
-
-//     try {
-//         const newOrder = await dbOrder.create({
-//             customerEmail: order_object.email,
-//             totalPrice: finalPrice,
-//             orderDate: today
-//         })
-
-//     } catch(err) {
-//         throw (err);
-//     }
-// }
-
-// const createOrderItem = async (order_object, new_order) => {
-//     try {
-//         let items = order_object.items;
-
-//         for ( let i = 0; i < items; i++) {
-//             const rowPrice = await (items[i].quantity * items[i].unitPrice);
-
-//             const newOrderItem = await dbOrderItem.create({
-//                 description: items[i].description,
-//                 quantity: items[i].quantity,
-//                 unitPrice: items[i].unitPrice,
-//                 rowPrice: rowPrice,
-//                 orderId: new_order._id
-//             })
-//         }
-//         return; //TODO: should we return something or just leave this?
-
-//     } catch (err) {
-//         throw(err);
-//     }
-// }
-
-
-
-// (async () => {
-//     const connection = await createMessageBrokerConnection();
-//     const channel = await createChannel(connection);
-
-//     //configureMessageBroker(channel);
-
-//     const { order } = messageBrokerInfo.exchanges;
-//     const { orderQueue } = connection.queues;
-//     const { output } = messageBrokerInfo.routingKeys;
-
-
-//     // TODO: Setup consumer
-//     channel.consume(orderQueue, data => {
-//         const dataJson = JSON.parse(data.content.toString()); //the order to be put into the database
-//         console.log(dataJson)
-//         //creates an order in mongodb
-//         const newOrder = createOrder(dataJson);
-
-//         //creates orderItems in mongodb
-//         const newOrderItem = createOrderItem(dataJson);
-
-//         channel.publish(order, output, new Buffer(JSON.stringify(newOrder)));
-
-//         console.log(`[x] Sent: ${JSON.stringify(dataJson)}`);
-//         //channel.publish(order, output, new Buffer(JSON.stringify(newOrderItem)));
-//     }, { noAck: true});
-
-
-// })().catch(e => console.error(e));
